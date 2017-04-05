c64ed3efb707e924113444d574693cafc0f7dea6
spallas <davide.spallaccini@gmail.com>
2017-04-05 19:08:45 +0200

    Improvments. Documentation update.

--- shared/semaphore.c
+++ shared/semaphore.c
@@ -2,7 +2,7 @@
 
 int sem_init(int init_value, int n){
 	int id_sem = semget(IPC_PRIVATE, n, IPC_CREAT|IPC_EXCL|0666);
-	if ( id_sem == -1 ) printf("Errore nella chiamata semget");
+	if ( id_sem == -1 ) LOGe("Error in semget at sem_init()");
 
 	union semun arg;
 	unsigned short arr[n];
@@ -12,7 +12,7 @@ int sem_init(int init_value, int n){
 	arg.array = arr;
 
 	int ret = semctl(id_sem, 0, SETALL, arg);
-	if(ret == -1) printf("Error initializing sem");
+	if(ret == -1) LOGe("Error initializing sem in sem_init()");
 	return id_sem;
 }
 
@@ -22,7 +22,7 @@ void sem_wait(int sem, int index){
 	op.sem_op = -1;
 	op.sem_flg = 0;
 	int ret = semop(sem, &op, 1);
-	if(ret == -1) printf("Error waiting sem");
+	if(ret == -1) LOGe("Error waiting sem");
 }
 
 void sem_post(int sem, int index){
@@ -31,13 +31,13 @@ void sem_post(int sem, int index){
 	op.sem_op = 1;
 	op.sem_flg = 0;
 	int ret = semop(sem, &op, 1);
-	if(ret == -1) printf("Error posting sem");
+	if(ret == -1) LOGe("Error posting sem");
 }
 
 
 int mutex_init(int n){
 	int mutex_id = semget(IPC_PRIVATE, n, IPC_CREAT|IPC_EXCL|0666);
-	if( mutex_id == -1) printf("Error initializing mutex");
+	if( mutex_id == -1) LOGe("Error initializing mutex");
 
 	union semun arg;
 	unsigned short arr[n];
@@ -47,7 +47,7 @@ int mutex_init(int n){
 	arg.array = arr;
 
 	int ret = semctl(mutex_id, 0, SETALL, arg);
-	if(ret == -1) printf("Error initializing mutex");
+	if(ret == -1) LOGe("Error initializing mutex");
 
 	return mutex_id;
 }
@@ -58,7 +58,7 @@ void mutex_lock(int mutex, int index){
 	op.sem_op = -1;
 	op.sem_flg = 0;
 	int ret = semop(mutex, &op, 1);
-	if(ret == -1) printf("Error locking mutex");
+	if(ret == -1) LOGe("Error locking mutex");
 }
 
 void mutex_unlock(int mutex, int index){
@@ -67,13 +67,17 @@ void mutex_unlock(int mutex, int index){
 	op.sem_op = 1;
 	op.sem_flg = 0;
 	int ret = semop(mutex, &op, 1);
-	if(ret == -1) printf("Errore unlocking mutex");
+	if(ret == -1) LOGe("Errore unlocking mutex");
 }
 
 void sem_close(int sem){
 	int index = 0;
 	union semun arg = {0};
 	int ret = semctl(sem, index, IPC_RMID, arg);
-	if(ret == -1) printf("Error closing sem: %s, %d, %d\n",
+	if(ret == -1) {
+		char info[64];
+		sprintf(info, "Error closing sem: %s, %d, %d\n",
 						 strerror(errno), sem, index);
+		LOGe(info);
+	}
 }
